bb8c5da3f92d004c07b9988771759ab5d93eeb92
Seyi Onifade <xyluz@ymail.com>
2019-05-29 20:37:23 +0100

    fixed newsletter thing

--- resources/views/layout/prices.blade.php
+++ resources/views/layout/prices.blade.php
@@ -1,6 +1,6 @@
 <section style="background:#fff;" class="font-sans container m-auto max-w-xl flex flex-col flex-wrap sm:flex-row justify-center mt-8 ">
             <h1 class="text-lg text-black items-center mt-8 text-black font-termina-m text-center container tracking-wide font-smooth   mb-6 text-2xl uppercase">Packages </h1>
-            <div style="border-right:1px solid #f06d46;" class="w-full sm:w-1/3 lg:w-1/3 text-center py-8 bg-white h-84 ">
+            <div style="border-right:1px solid #f06d46;" class="w-full sm:w-1/3 lg:w-1/3 text-center py-8 bg-white h-85 ">
                 <p class="text-center mb-7"><img width="200px" src="img/dance_the_music.svg"></p>
                 <label for="" class="uppercase tracking-loose font-bold text-lg font-smooth font-termina-m ">Clout</label>
                 <h1 style="color: #fc005e" for="" class="uppercase tracking-loose font-smooth font-bold text-5xl my-2 font-termina-m">N150,000</h1>
@@ -10,12 +10,12 @@
                 </button>
                 <p style="width: 50%" class="text-center m-auto  text-m mt-4 mb-2 font-termina-m"> Promotion Available in Lagos ONLY </p>
             </div>
-            <div style="border-right:1px solid #f06d46; border-bottom:1px solid #f06d46;" class="w-full sm:w-1/3 lg:w-1/3 text-center bg-white  h-84">
+            <div style="border-right:1px solid #f06d46; border-bottom:1px solid #f06d46;" class="w-full sm:w-1/3 lg:w-1/3 text-center bg-white  h-85">
                 <p class="text-white bg-orang  mb-6 font-smooth font-termina-m p-2 uppercase"> Best Value</p>
                 <p class="text-center mb-4"><img width="220px" src="img/balloon.svg"></p>
                 <label for="" class="uppercase tracking-loose font-bold text-lg font-termina-m font-smooth">More Clout</label>
                 <h1 style="color: #0087fc" for="" class="uppercase tracking-loose font-bold text-5xl my-2 font-smooth font-termina-m ">   N200,000 <label for="" class="capitalize tracking-loose font-bold text-lg font-termina-m font-smooth block">25% off for Gospel Artistes</label></h1>
-                <p class="my-2 text-grey-darker font-termina-l pl-6 pr-6 mb-4 w-74 m-auto text-lg leading-tight">If you are looking to gain clout with your music with audiences from Lagos, Port-harcourt, Onitsha, Abuja & Kano</p>
+                <p class="my-2 text-grey-darker font-termina-l pl-6 pr-6 mb-4 w-74 m-auto text-lg leading-tight">If you are looking to gain clout with your music with audiences from Lagos, Port-harcourt, Onitsha, Abuja &amp; Kano</p>
 
                 <button style="background:#0087fc;" class="butn btn-4 bg-transparent hover:bg-black font-smooth text-black-dark font-termina-m uppercase hover:text-white py-2 px-4 border border-black hover:border-transparent">
                     Buy Now
@@ -24,7 +24,7 @@
                 <p class="text-center text-m mt-2 mb-2 font-termina-m"> Network-Syndicated Radio Interview </p>
                 <p class="text-center text-m mt-2 mb-4 font-termina-m"> Digitally-Syndicated Spotlight feature </p>
             </div>
-            <div class="w-full sm:w-1/3 lg:w-1/3 text-center py-8 bg-white  h-84">
+            <div class="w-full sm:w-1/3 lg:w-1/3 text-center py-8 bg-white  h-85">
                 <p class="text-center mb-9"><img width="250px" src="img/watering_plant.svg"></p>
                 <label for="" class="uppercase tracking-loose text-lg font-termina-m font-smooth p-2">For Growing Businesses</label>
                 <h1 style="color:#f06c46" for="" class="uppercase tracking-loose font-bold text-5xl my-2 font-smooth font-termina-m">Business</h1>
@@ -36,16 +36,13 @@
             </div>
 
         </section>
-
-<section style="background-image: url('https://aux.ng/img/317a3.jpg') !important; background-repeat: no-repeat; background-size: cover; background-position: center; box-shadow: rgba(20, 20, 20,.5) 625px 10px 108px 6px inset; padding-top: 0px !important;"  class="h-screen-r text-white m-auto container mt-8"><br><br> <div style="margin-top: 5em;" class="container m-auto flex flex-col md:flex-row max-w-xl  py-8 ">
-  <br><br><br><br><br><br>  <div class="flex flex-col w-full  justify-center  py-8 px-4"> 
-  
- <div class="hi-icon-effect-8 text-center  ">    <a  id="hi-icon"  href="#set-8" class="hi-icon mdi-play-circle"></a> <p class="mt-4 font-termina-m uppercase tracking-wide"> Watch Spotlight</p> </div>
-
-  
-
+<section style="background:#f78b2754; " class="antialiased font-sans w-full bg-white text-left text-black py-8 container m-auto p-8">
+  <div class="container mx-auto max-w-xl py-8 font-normal ">
+    <h3 style="width: 40%" class="text-4xl font-termina-m">Get the goodies delivered straight to your inbox </h3>
+    <p class="font-termina-l text-grey-darkest text-xl mt-2 mb-2">Subscribe to the newsletter</p>  <img class="img-news" src="img/newsletter.webp">
+    <div class="max-w-sm mt-4 sm:flex">
+      <input type="email" class="block font-termina-l w-full focus:outline-0 bg-white py-3 px-6 mb-2 sm:mb-0" name="email" placeholder="Enter your email" required="">
+      <button style="height: 50px" class="uppercase text-sm text-white focus:outline-0 w-full sm:w-auto bg-black hover:bg-grey-darkest focus:bg-grey-light tracking-wide px-6 font-termina-md">Subscribe</button>
     </div>
-  
-
-
-</section> 
+  </div>
+</section>
