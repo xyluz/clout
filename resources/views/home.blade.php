dfacc9e6ec2ad5569ccae7898c9c31ffa85ac03d
Seyi Onifade <xyluz@ymail.com>
2019-09-02 02:19:57 +0100

    home page fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,17 +9,21 @@
             <div class="flex flex-col w-full lg:w-1/2  justify-center  py-2 px-4">
 
                 <div class="hi-icon-effect-8    ">
-                    <h1 style="z-index: 1" class="c-title__name font-termina-m mb-4 text-5xl px-6 w-h">Put your brand power in motion with Clout.  </h1>
+                    <h1 style="z-index: 1" class="c-title__name font-termina-m mb-4 text-5xl px-6 w-h">Put your brand power in motion. Reach out with Clout.  </h1>
 
                     <p style="z-index: 1" class="font-termina-l text-2xl leading-tight pr-4 px-6 mb-4 antialiased w-h"> We offer packages that help artistes and small business get more value & trackable milage for their brand and business at the most affordable rates .</p>
                 </div>
-                @auth 
+                {{-- @auth 
             <a href="{{route('dashboard')}}" style="width: 201px; margin-left: 1em; padding-left: 53px;" class="p-4 font-termina-md rounded-full text-black bg-white">Dashboard</a>
             @else 
             <a href="{{route('register')}}" style="width: 201px; margin-left: 1em; padding-left: 53px;" class="p-4 font-termina-md rounded-full text-black bg-white">Get Started</a>
-            @endauth
+            @endauth --}}
+
+            <button class="bg-white p-4 text-center font-seg rounded-full w-2/4"> For Artistes</button><br>
+            <button class="bg-white p-4 text-center font-seg rounded-full w-2/4"> For Businesses</button>
 
             </div>
+            
 
     </section>
  
---
+++ resources/views/layout/begin.blade.php
@@ -0,0 +1,18 @@
+<section class="bg-grey-lightest py-4 font-sans">
+    <div class=" container max-w-xl m-auto flex flex-wrap flex-col md:flex-row items-center justify-start">
+      <div class="w-full   p-3">
+        <div class="flex flex-col lg:flex-row  overflow-hidden h-auto lg:h-36 border shadow bg-white ">
+          <img class="block  w-full lg:w-64 flex-none bg-cover h-prod "
+            src="img/fem.webp">
+          <div class="bg-white  p-4 flex flex-col justify-between leading-normal h-auto bg-white">
+              <div class="text-grey-dark uppercase text-sm tracking-wide lg:w-2/3 leading-tight font-seg antialiased mb-1 ">Begin your Journey</div>
+            <div class="text-black font-medium text-2xl lg:w-2/3  mb-2 leading-tight font-seg ">Produce your next single, mixtape or album with us</div>
+          <a href="{{route('package')}}"> <p style="color: #e80016;" class="text-grey-darker text-base font-seg antialiased">See Packages </p>
+          </div>
+        </div>
+      </div>
+   
+      </div>
+    </div>
+  </section>
+  
