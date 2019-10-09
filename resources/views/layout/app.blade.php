a4664f65030a1bd14f2d3ea20817ca5b969de500
Seyi Onifade <xyluz@ymail.com>
2019-10-09 12:44:17 +0100

    logout

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -74,8 +74,17 @@
 
         <ul id="cd-navigation">
             @auth
-            <li><a href="{{route('dashboard')}}">Dashboard</a></li>
+            <li>
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+                </a>
 
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </li>
+            <li><a href="{{route('dashboard')}}">Dashboard</a></li>
+           
             @else 
 
             
